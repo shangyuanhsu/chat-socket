feda1c89de670714a9a0ef2b7de69d96eb639e75
shang <shang@cl.ncu.edu.tw>
2021-10-19 14:42:08 +0800

    ok

--- index.js
+++ index.js
@@ -47,7 +47,8 @@
 
 const app = require('express')();
 const http = require('http').Server(app);
-const io = require('socket.io')(http);
+var io = require('socket.io')(server); 
+// const io = require('socket.io')(http);
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
