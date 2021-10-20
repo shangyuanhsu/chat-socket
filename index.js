2754cb3f2cb205ed0fbf1e9883cf6d490ebc9a8c
shang <shang@cl.ncu.edu.tw>
2021-10-20 13:45:57 +0800

    ss

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
 const app = require('express')();
 const http = require('http').Server(app);
-const io = require('socket.io')(http);
+// const io = require('socket.io')(http);
+const io = require("socket.io")(http, {
+    cors: {
+      origin: "*",
+      methods: ["GET", "POST"]
+    }
+  });
 const port = process.env.PORT || 3000;
 
 
