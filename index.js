1845c8783d3111205e5d28016e815c74863283e9
shang <shang@cl.ncu.edu.tw>
2021-10-20 13:49:42 +0800

    ss

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ const http = require('http').Server(app);
 // const io = require('socket.io')(http);
 const io = require("socket.io")(http, {
     cors: {
-      origin: "*",
-      methods: ["GET", "POST"]
+      origin: "*"
     }
   });
 const port = process.env.PORT || 3000;
