3a93de538106cb2d53cf23c7fe0bf314806bb796
shang <shang@cl.ncu.edu.tw>
2021-10-20 09:50:52 +0800

    s

--- index.js
+++ index.js
@@ -3,21 +3,12 @@ const http = require('http').Server(app);
 const io = require('socket.io')(http);
 const port = process.env.PORT || 3000;
 
-const socket = require("socket.io-client")("https://shangyuanhsu.github.io/chat-example/");
-
-socket.on("connect_error", (err) => {
-  console.log(`connect_error due to ${err.message}`);
-});
-
 
 
 app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
-app.get('/socket.io/socket.io.js', (req, res) => {
-    res.sendFile(__dirname + '/socket.io/socket.io.js');
-});
 
 
 io.on('connection', (socket) => {
